8036b4d767218170f6d8d0539681079c7c11f31d
ZR74 <2401889661@qq.com>
2025-12-08 08:35:33 +0000

    feat: remove ignored test cases for EVM multipass

--- tools/run_evm_tests.py
+++ tools/run_evm_tests.py
@@ -51,10 +51,6 @@ class TestRunner:
 
     # TODO: Failed cases in EVM multipass, temporarily ignored
     IGNORE_CASES = {
-        "add_stack_underflow.evm.hex", # stack exception handle
-        "invalid_jump.evm.hex",        # exception handle
-        "invalid_jumpi.evm.hex",       # exception handle
-        "stack_overflow.evm.hex",      # stack exception handle
     }
 
     def __init__(self, args):
